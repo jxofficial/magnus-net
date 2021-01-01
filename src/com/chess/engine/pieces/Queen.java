b46c942d821b42c79fca6e204715e4394b73434b
JXKENG <jxkeng119@gmail.com>
2021-01-01 21:51:04 +0800

    add Queen class

---
+++ src/com/chess/engine/pieces/Queen.java
@@ -0,0 +1,61 @@
+package com.chess.engine.pieces;
+
+import com.chess.engine.Alliance;
+import com.chess.engine.board.Board;
+import com.chess.engine.board.BoardUtils;
+import com.chess.engine.board.Move;
+import com.chess.engine.board.Tile;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class Queen extends Piece {
+    // combination of both rook and bishop
+    private static final int[] CANDIDATE_MOVE_VECTOR_COORDINATES = {-9, -8, -7, -1, 1, 7, 8, 9};
+
+    public Queen(int piecePosition, Alliance pieceAlliance) {
+        super(piecePosition, pieceAlliance);
+    }
+
+    @Override
+    public Collection<Move> calculateLegalMoves(Board board) {
+        final List<Move> legalMoves = new ArrayList<>();
+
+        for (final int candidateOffset : CANDIDATE_MOVE_VECTOR_COORDINATES) {
+            int candidateDestinationCoordinate = this.piecePosition;
+
+            while (BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)) {
+                if (isFirstColumnExclusion(candidateDestinationCoordinate, candidateOffset)
+                        || isEighthColumnExclusion(candidateDestinationCoordinate, candidateOffset)
+                )
+
+                    candidateDestinationCoordinate += candidateOffset;
+                if (BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)) {
+                    final Tile candidateDestinationTile = board.getTile(candidateDestinationCoordinate);
+                    if (!candidateDestinationTile.isTileOccupied()) {
+                        legalMoves.add(new Move.PieceMove(board, this, candidateDestinationCoordinate));
+                    } else {
+                        final Piece pieceAtDestination = candidateDestinationTile.getPiece();
+                        final Alliance alliance = pieceAtDestination.getPieceAlliance();
+                        if (this.pieceAlliance != alliance) {
+                            legalMoves.add(new Move.CapturingMove(board, this, candidateDestinationCoordinate, pieceAtDestination));
+                        }
+                        break; // piece is blocking bishop, no need to consider diagonal further
+                    }
+                }
+            }
+        }
+
+        return legalMoves;
+    }
+
+    // combination of both rook and bishop
+    private static boolean isFirstColumnExclusion(int currentPosition, int candidateOffset) {
+        return BoardUtils.FIRST_COLUMN[currentPosition] && (candidateOffset == -9 || candidateOffset == 7 || candidateOffset == -1);
+    }
+
+    private static boolean isEighthColumnExclusion(int currentPosition, int candidateOffset) {
+        return BoardUtils.EIGHTH_COLUMN[currentPosition] && (candidateOffset == -7 || candidateOffset == 9 || candidateOffset == 1);
+    }
+}
