c59b761d6137d397f47dca5f41c4dd3f52aaa166
JXKENG <jxkeng119@gmail.com>
2021-02-28 11:37:21 +0800

    display pieces on board

--- src/com/chess/gui/Table.java
+++ src/com/chess/gui/Table.java
@@ -7,12 +7,16 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Table {
     private final JFrame gameFrame;
     private final BoardPanel boardPanel;
+    private final Board chessBoard;
+
     private static final Dimension OUTER_FRAME_DIMENSION = new Dimension(600, 600);
     private static final Dimension BOARD_PANEL_DIMENSION = new Dimension(400, 350);
     private static final Dimension TILE_PANEL_DIMENSION = new Dimension(10, 10);
@@ -20,12 +24,13 @@ public class Table {
     private final Color DARK_TILE_COLOR = Color.decode("#769656");
 
     public Table() {
+        this.chessBoard = Board.createStandardBoard();
+
         this.gameFrame = new JFrame("magnus-net");
         this.gameFrame.setLayout(new BorderLayout());
         final JMenuBar tableMenuBar = createTableMenuBar();
         this.gameFrame.setJMenuBar(tableMenuBar);
         this.gameFrame.setSize(OUTER_FRAME_DIMENSION);
-
         this.boardPanel = new BoardPanel();
         this.gameFrame.add(boardPanel, BorderLayout.CENTER);
 
@@ -60,16 +65,16 @@ public class Table {
         final List<TilePanel> boardTiles;
 
         BoardPanel() {
-             super(new GridLayout(8,8));
-             this.boardTiles = new ArrayList<>();
-             for (int i = 0; i < BoardUtils.NUM_TILES; i++) {
-                 final TilePanel tilePanel = new TilePanel(this, i);
-                 this.boardTiles.add(tilePanel);
-                 this.add(tilePanel);
-             }
-             setPreferredSize(BOARD_PANEL_DIMENSION);
-             validate();
-         }
+            super(new GridLayout(8, 8));
+            this.boardTiles = new ArrayList<>();
+            for (int i = 0; i < BoardUtils.NUM_TILES; i++) {
+                final TilePanel tilePanel = new TilePanel(this, i);
+                this.boardTiles.add(tilePanel);
+                this.add(tilePanel);
+            }
+            setPreferredSize(BOARD_PANEL_DIMENSION);
+            validate();
+        }
     }
 
     private class TilePanel extends JPanel {
@@ -80,16 +85,43 @@ public class Table {
             this.tileCoordinate = tileCoordinate;
             setPreferredSize(TILE_PANEL_DIMENSION);
             assignTileColor();
+            assignTilePieceIcon(chessBoard);
             validate();
         }
 
+        private void assignTilePieceIcon(final Board board) {
+            final String localDir = System.getProperty("user.dir");
+            final String PIECE_ICON_ROOT_PATH = "\\art\\pieces\\";
 
-//        private void assignTilePieceIcon(final Board board) {
-//            this.removeAll();
-//            if (board.getTile(this.tileCoordinate).isTileOccupied()) {
-//                final BufferedImage img = ImageIO.read(new File())
-//            }
-//        }
+            this.removeAll();
+            if (board.getTile(this.tileCoordinate).isTileOccupied()) {
+
+                String pieceAlliancePrefix = board
+                        .getTile(this.tileCoordinate)
+                        .getPiece()
+                        .getPieceAlliance()
+                        .toString()
+                        .substring(0, 1);
+
+                String pieceName = board
+                        .getTile(this.tileCoordinate)
+                        .getPiece()
+                        .toString();
+
+                // eg art/basic-pieces/BK.gif for black king
+                String iconFilePath = localDir + PIECE_ICON_ROOT_PATH
+                        + pieceAlliancePrefix + pieceName + ".gif";
+                try {
+                    final BufferedImage img = ImageIO.read(
+                            new File(iconFilePath));
+                    add(new JLabel(new ImageIcon(img)));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+
         private void assignTileColor() {
             // where ever I am, I + my row
             boolean isLightTile = ((tileCoordinate + tileCoordinate / 8) % 2 == 0);
